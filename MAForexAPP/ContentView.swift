9adcc7fd1085068963c91dd6c5ee7622ceb95b42
Jamie Winfield <jamie.winfield@live.co.uk>
2022-05-06 22:10:51 +0100

    added AppPear/ChartView library so that i can use better charts to display data and implemented a view switch so that when you click on a pair in the list it takes you to the chart showing the previous 30 days

--- MAForexAPP/ContentView.swift
+++ MAForexAPP/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import Foundation
+import Combine
+import SwiftUICharts
 
 // github token = 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
 public struct Data: Decodable
@@ -26,6 +28,18 @@ public struct Rates: Decodable
     let end_rate: Double?
     let change: Double?
     let change_pct: Double?
+}
+
+public struct TimeSeries: Decodable
+{
+    let base: String?
+    let success: Bool?
+    let start_date: String?
+    let end_date: String?
+    let rates: [String: [String: Double]]?
+    var keys: Array<String>?
+    var dates: Array<String>?
+    var data: [String: Array<Double>]?
 }
 
 
@@ -53,17 +67,36 @@ let json = """
     "success":true
 }
 """
-
+public func GetDate(day: Int) -> String
+{
+    var dayComponent = DateComponents()
+    dayComponent.day = day
+    let calendar = Calendar.current
+    let date = calendar.date(byAdding: dayComponent, to: Date())!
+    let formatter = DateFormatter()
+    formatter.locale = .current
+    formatter.dateFormat = "yyyy-MM-dd"
+    return formatter.string(from: date)
+}
 public final class WebService : NSObject, ObservableObject
 {
     
     var refreshingDict: [String: Bool] = ["EUR" : true, "USD" : true, "GBP": true, "CAD" : true, "AUD" : true, "NZD" : true, "CHF" : true]
+    var refreshingDictTime: [String: Bool] = ["EUR" : true, "USD" : true, "GBP": true, "CAD" : true, "AUD" : true, "NZD" : true, "CHF" : true]
     @Published var refreshing: Bool = true
+    @Published var refreshingTime: Bool = true
     let defaults = UserDefaults.standard
     var result = "no result"
     
+    let nodata: Bool = false
+    
     @Published var allApiData: Array<Data> = []
     var newApiData: Array<Data> = []
+    
+    @Published var timeseriesData: Array<TimeSeries> = []
+    @Published var timeseriesDataDict: [String: TimeSeries] = [:]
+    var newTimeseriesData: Array<TimeSeries> = []
+    
     let EURSymbols = "USD,JPY,GBP,AUD,CAD,NZD,CHF,HKD"
     let USDSymbols =  "JPY,CAD,CHF"
     let GBPSymbols = "USD,JPY,AUD,CAD,CHF,HKD"
@@ -77,6 +110,11 @@ public final class WebService : NSObject, ObservableObject
     // base pairs which should be supported =
     // EUR / USD / GBP / CAD / AUD / NZD / CHF / ZAR? / SGD?
     
+    enum RequestType
+    {
+        case FLUCTUATION, TIMESERIES
+    }
+    
     
     public override init()
     {
@@ -87,8 +125,8 @@ public final class WebService : NSObject, ObservableObject
         
         for base in BaseSymbols
         {
-            let _url = "https://api.apilayer.com/fixer/fluctuation?&symbols=" + QuoteSymbols[base]! + "&base=" + base
-            let _saved = defaults.string(forKey: base + "Data")
+           
+            var _saved = defaults.string(forKey: base + "Data")
             if(_saved != nil)
             {
                 let data = _saved!.data(using: .utf8)!
@@ -98,10 +136,75 @@ public final class WebService : NSObject, ObservableObject
                     self.allApiData.append(_decoded!)
                 }
             }
-            MakeDataRequest(url: _url, base: base)
+            
+            _saved = defaults.string(forKey: base + "TimeData")
+            if(_saved != nil)
+            {
+                let data = _saved!.data(using: .utf8)!
+                let _decoded = try? JSONDecoder().decode(TimeSeries.self, from: data)
+                if(_decoded != nil)
+                {
+                    self.timeseriesData.append(_decoded!)
+                    self.timeseriesDataDict["base"] = _decoded
+                }
+            }
+            if(!nodata)
+            {
+                var _url = "https://api.apilayer.com/fixer/fluctuation?start_date=" + GetDate(day: -1) + "&end_date=" + GetDate(day: 0) + "&symbols=" + QuoteSymbols[base]! + "&base=" + base
+                MakeDataRequest(url: _url, base: base, type: .FLUCTUATION)
+                _url = "https://api.apilayer.com/fixer/timeseries?start_date=" + GetDate(day: -30) + "&end_date=" + GetDate(day: 0) + "&symbols=" + QuoteSymbols[base]! + "&base=" + base
+                MakeDataRequest(url: _url, base: base, type: .TIMESERIES)
+                
+            }
+            else
+            {
+                DispatchQueue.main.async {
+                    self.refreshingTime = false
+                    self.refreshing = false
+                }
+            }
             
         }
         SetKeys()
+        SetDates()
+        
+    }
+    
+    
+    
+    
+    
+    private func SetDates()
+    {
+        DispatchQueue.main.async
+        {
+            for index in self.timeseriesData.indices
+            {
+                self.timeseriesData[index].dates = []
+                self.timeseriesData[index].keys = []
+                self.timeseriesData[index].data = [:]
+                
+                for _date in self.timeseriesData[index].rates!.keys
+                {
+                    self.timeseriesData[index].dates!.append(_date)
+                    
+                }
+                for _key in self.timeseriesData[index].rates![GetDate(day: 0)]!.keys
+                {
+                    self.timeseriesData[index].keys!.append(_key)
+                    var _data: [Double] = []
+                    for i in -30...0
+                    {
+                        _data.append(self.timeseriesData[index].rates![GetDate(day: i)]![_key]!)
+                    }
+                    self.timeseriesData[index].data![_key] = _data
+                    //dump(self.timeseriesData[index].data![_key]!)
+                }
+                self.timeseriesDataDict[self.timeseriesData[index].base!] = self.timeseriesData[index]
+            }
+            //dump(self.timeseriesData[0].data!["USD"]!)
+            
+        }
         
     }
     
@@ -125,25 +228,32 @@ public final class WebService : NSObject, ObservableObject
     public func RefreshData()
     {
         self.newApiData = []
+        self.newTimeseriesData = []
         
         for key in self.refreshingDict.keys
         {
             
             self.refreshingDict[key] = true
         }
+        for key in self.refreshingDictTime.keys
+        {
+            self.refreshingDictTime[key] = true
+        }
         refreshing = true
+        refreshingTime = true
         for base in BaseSymbols
         {
-            let _url = "https://api.apilayer.com/fixer/fluctuation?&symbols=" + QuoteSymbols[base]! + "&base=" + base
-            MakeDataRequest(url: _url, base: base)
-            
+            var _url = "https://api.apilayer.com/fixer/fluctuation?start_date=" + GetDate(day: -1) + "&end_date=" + GetDate(day: 0) + "&symbols=" + QuoteSymbols[base]! + "&base=" + base
+            MakeDataRequest(url: _url, base: base, type: .FLUCTUATION)
+            _url = "https://api.apilayer.com/fixer/timeseries?start_date=" + GetDate(day: -30) + "&end_date=" + GetDate(day: 0) + "&symbols=" + QuoteSymbols[base]! + "&base=" + base
+            MakeDataRequest(url: _url, base: base, type: .TIMESERIES)
         }
         
         
         
     }
     
-    public func MakeDataRequest(url: String, base: String)
+    func MakeDataRequest(url: String, base: String, type: RequestType)
     {
         var request = URLRequest(url: URL(string: url)!,timeoutInterval: Double.infinity)
         
@@ -160,50 +270,95 @@ public final class WebService : NSObject, ObservableObject
             
             self.result = String(data: data, encoding: .utf8)!
             print(self.result)
-            let _data = try? JSONDecoder().decode(Data.self, from: data)
-            if(_data != nil)
+            
+            switch (type)
             {
-                
-                if (_data!.success != nil && _data!.success!)
-                {
-                    self.defaults.set(self.result, forKey:  base+"Data")
-                    self.newApiData.append(_data!)
-                    
-                }
-                
-            }
-            
-            
-                
-            self.refreshingDict[base] = false
-            
-            var _refresh = false
-            for refresh in self.refreshingDict
-            {
-                if(refresh.value)
-                {
-                   _refresh = true
-                }
-            }
-            
-            if (!_refresh)
-            {
-                DispatchQueue.main.async
-                {
-                    if (self.newApiData.isEmpty == false)
+            case .FLUCTUATION:
+                do {
+                    let _data = try? JSONDecoder().decode(Data.self, from: data)
+                    if(_data != nil)
                     {
-                        self.allApiData = self.newApiData
+                        
+                        if (_data!.success != nil && _data!.success!)
+                        {
+                            self.defaults.set(self.result, forKey:  base+"Data")
+                            self.newApiData.append(_data!)
+                            
+                        }
                         
                     }
-                    self.newApiData = []
-                    self.SetKeys()
-                    self.refreshing = _refresh
+                    
+                    
+                        
+                    self.refreshingDict[base] = false
+                    
+                    var _refresh = false
+                    for refresh in self.refreshingDict
+                    {
+                        if(refresh.value)
+                        {
+                           _refresh = true
+                        }
+                    }
+                    
+                    if (!_refresh)
+                    {
+                        DispatchQueue.main.async
+                        {
+                            if (self.newApiData.isEmpty == false)
+                            {
+                                self.allApiData = self.newApiData
+                                
+                            }
+                            self.newApiData = []
+                            self.SetKeys()
+                            self.refreshing = _refresh
+                        }
+                        
+                    }
+                    
                 }
-                
+            case .TIMESERIES:
+                do {
+                    let _data = try? JSONDecoder().decode(TimeSeries.self, from: data)
+                    
+                    if (_data != nil)
+                    {
+                        if (_data!.success != nil && _data!.success!)
+                        {
+                            self.defaults.set(self.result, forKey:  base+"TimeData")
+                            self.newTimeseriesData.append(_data!)
+                            
+                        }
+                    }
+                    
+                    self.refreshingDictTime[base] = false
+                    var _refresh = false
+                    
+                    for refresh in self.refreshingDictTime
+                    {
+                        if (refresh.value)
+                        {
+                            _refresh = true
+                        }
+                    }
+                    if(!_refresh)
+                    {
+                        
+                       
+                        DispatchQueue.main.async
+                        {
+                            if(self.newTimeseriesData.isEmpty == false)
+                            {
+                                self.timeseriesData = self.newTimeseriesData
+                                self.SetDates()
+                            }
+                            self.newTimeseriesData = []
+                            self.refreshingTime = false
+                        }
+                    }
+                }
             }
-            
-            
-            
         }
         
         task.resume()
@@ -255,6 +410,75 @@ struct Row: View{
     }
 }
 
+struct LineShape: Shape
+{
+    var yValues: [Double]
+    
+    func path(in rect: CGRect) -> Path
+    {
+        let xIncrement = (rect.width / (CGFloat(yValues.count) - 1))
+        let factor = rect.height / CGFloat(yValues.max() ?? 1.0)
+        var path = Path()
+        path.move(to: CGPoint(x: 0.0, y: rect.height - (yValues[0] * factor)))
+        for i in 1..<yValues.count
+        {
+            let pt = CGPoint(x: (Double(i) * Double(xIncrement)), y: (rect.height - (yValues[i] * factor)))
+            path.addLine(to: pt)
+        }
+        return path
+    }
+    
+}
+
+struct LineChartView1: View
+{
+    var title: String
+    var data: [Double]
+    
+    var body: some View
+    {
+        GeometryReader
+        {
+            gr in
+            let headHeight = gr.size.height * 0.10
+            VStack
+            {
+                ChartHeaderView(title: title, height: headHeight)
+                ChartAreaView(data: data)
+            }
+        }
+    }
+}
+
+struct ChartHeaderView: View
+{
+    var title: String
+    var height: CGFloat
+    
+    var body: some View
+    {
+        Text(title)
+            .frame(height: height)
+    }
+}
+
+struct ChartAreaView: View
+{
+    var data: [Double]
+    
+    var body: some View
+    {
+        ZStack
+        {
+            RoundedRectangle(cornerRadius: 3.0)
+                .fill(Color(#colorLiteral(red: 0.8906, green: 0.9005, blue: 0.82088, alpha: 1)))
+            
+            LineShape(yValues: data)
+                .stroke(Color.red, lineWidth: 2.0)
+        }
+    }
+}
+
 struct ContentView: View {
     @ObservedObject var service = WebService()
     
@@ -278,19 +502,26 @@ struct ContentView: View {
                     ProgressView()
                 }
             }
-        List
-        {
-            
-            ForEach(service.allApiData.indices, id: \.self) { index in
-                if(service.allApiData[index].keys != nil)
+            NavigationView
+            {
+                List
                 {
-                    ForEach(service.allApiData[index].keys!, id: \.self) {key in
-                        Row(data: service.allApiData[index], key: key)
-                    }
-                }
-            }
             
-        }
+                    ForEach(service.allApiData.indices, id: \.self) { index in
+                        if(service.allApiData[index].keys != nil)
+                        {
+                            ForEach(service.allApiData[index].keys!, id: \.self) {key in
+                                let base = service.allApiData[index].base!
+                                NavigationLink(destination: LineView(data: service.timeseriesDataDict[base]!.data![key]!, title: base + " / " + key, valueSpecifier: "%.4f"))
+                                {
+                                    Row(data: service.allApiData[index], key: key)
+                                }
+                            }
+                        }
+                    }
+            
+                }
+            }.navigationTitle("Back")
         }
         
         
